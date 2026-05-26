bc84eacc2ac7db3d6fec444f8ba7cec971ed85ca
Kang-Woo Lee <kwlee@etri.re.kr>
2026-05-26 13:31:01 +0900

    .

--- src/main/java/lg/inspector/UpperImageUploader.java
+++ src/main/java/lg/inspector/UpperImageUploader.java
@@ -16,8 +16,7 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.common.base.Preconditions;
-
+import utils.Preconditions;
 import utils.async.AbstractLoopThreadService;
 import utils.io.FileUtils;
 
